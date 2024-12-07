9303f8fac8786837c8fa399f223e5e4eb4997d23
Eric Guinn <eric.guinn@outlook.com>
2024-12-06 19:56:58 -0700

    Day 6 scaffold

---
+++ day06/main.cc
@@ -0,0 +1,7 @@
+#include "lib.hh"
+
+int Logger::LogLevel = 0;
+
+int main() {
+    auto contents = ReadEntireFile("day06/input.txt");
+}
---
+++ day06/test.cc
@@ -0,0 +1,7 @@
+#include "lib.hh"
+
+int Logger::LogLevel = 4;
+
+int main() {
+    auto contents = ReadEntireFile("day06/ex01.txt");
+}
